c3cdc6204a4c509c0f07cf2b1b3df6ecd2b4fd96
sspeedwagonn <burnerforcarter@gmail.com>
2025-02-06 20:55:24 -0600

    Setting up immune players + independent command file

--- src/main/java/net/civicraft/mutualDemise/MutualDemise.java
+++ src/main/java/net/civicraft/mutualDemise/MutualDemise.java
@@ -1,44 +1,46 @@
 package net.civicraft.mutualDemise;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 
 public final class MutualDemise extends JavaPlugin implements Listener, CommandExecutor {
+    public static MutualDemise instance;
+    List<Player> onlinePlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
+    List<String> uuidStrings = getConfig().getStringList("immune_players");
+    List<UUID> immuneUUIDs = uuidStrings.stream().map(UUID::fromString).toList();
 
     @Override
     public void onEnable() {
-        getLogger().info("Enabling MutualDemise");
+        instance = this;
+        getLogger().info("Enabling MutualDemise...");
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
-        Objects.requireNonNull(getCommand("mutualdemise")).setExecutor(this);
+        Objects.requireNonNull(getCommand("mutualdemise")).setExecutor(new MDCommand());
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Disabling MutualDemise");
+        instance = null;
+        getLogger().info("Disabling MutualDemise...");
+    }
+
+    public static MutualDemise getInstance() {
+        return instance;
     }
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         if (getConfig().getStringList("enabled_worlds").contains(event.getEntity().getWorld().getName())) {
             Player deadPlayer = event.getPlayer();
-            List<Player> onlinePlayers = new ArrayList<>(Bukkit.getOnlinePlayers());
             onlinePlayers.remove(deadPlayer);
+            removeImmunePlayers();
 
             if (getConfig().getBoolean("everyone_dies")) {
                 for (Player player : onlinePlayers) {
@@ -57,18 +59,12 @@ public final class MutualDemise extends JavaPlugin implements Listener, CommandE
         }
     }
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-            if (strings.length > 0 && strings[0].equalsIgnoreCase("reload")) {
-                if (commandSender.hasPermission("mutualdemise.reload")) {
-                    reloadConfig();
-                    commandSender.sendMessage(Component.text("[MutualDemise] config.yml has been reloaded! ☠").color(NamedTextColor.DARK_RED));
-                    return true;
-                } else {
-                    commandSender.sendMessage(Component.text("[MutualDemise] You do not have permission to use this command. ☠").color(NamedTextColor.DARK_RED));
-                    return false;
-                }
+    private void removeImmunePlayers() {
+        for (UUID uuid : immuneUUIDs) {
+            Player player = Bukkit.getPlayer(uuid);
+            if (player != null && player.isOnline()) {
+                onlinePlayers.remove(player);
             }
-        return false;
+        }
     }
 }
